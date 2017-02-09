d001eb28d7b0631e296213c973f858cae970a0d9
Paul Stuart <pauleyphonic@gmail.com>
2017-02-08 17:59:39 -0800

    fix cmd build

--- cmd/example.go
+++ cmd/example.go
@@ -5,35 +5,35 @@ import (
 	"fmt"
 	"net"
 
-    "github.com/ebookbug/gosnmptrap"
+	"github.com/paulstuart/gosnmptrap"
 )
 
 func main() {
 	fmt.Println("Hello World!")
-	socket,err := net.ListenUDP("udp4",&net.UDPAddr{
-		IP:net.IPv4(0,0,0,0),
-		Port:162,
+	socket, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 162,
 	})
-	if err !=nil{
+	if err != nil {
 		panic(err)
 	}
 	defer socket.Close()
-	
-	for{
-		buf := make([]byte,2048)
-		read,from,_:=socket.ReadFromUDP(buf)
-		fmt.Println("Get msg from ",from.IP)
-		go gosnmptrap.HandleUdp(buf[:read])
+
+	for {
+		buf := make([]byte, 2048)
+		read, from, _ := socket.ReadFromUDP(buf)
+		fmt.Println("Get msg from ", from.IP)
+		go HandleUdp(buf[:read])
 	}
 }
 
-func HandleUdp(data []byte){
-	trap,err := gosnmptrap.ParseUdp(data)
-	if err !=nil{
-		fmt.Println("Err",err.Error())
+func HandleUdp(data []byte) {
+	trap, err := gosnmptrap.ParseUdp(data)
+	if err != nil {
+		fmt.Println("Err", err.Error())
 	}
-	fmt.Println(trap.Version,trap.Community,trap.EnterpriseId,trap.Address)
-	for k,v :=range trap.Values{
-		fmt.Printf("%s = %s\n",k,v);
+	fmt.Println(trap.Version, trap.Community, trap.EnterpriseId, trap.Address)
+	for k, v := range trap.Values {
+		fmt.Printf("%s = %s\n", k, v)
 	}
 }
